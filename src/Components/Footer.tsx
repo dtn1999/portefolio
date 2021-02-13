1fb1566f9721736429786ce2daca1d225ae7867c
Danyls <danyngongang@gmail.com>
2021-02-13 03:03:00 +0100

    update footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -24,8 +24,7 @@ const Footer:React.FC<Props>= React.memo(({data})=>{
           </ul>
 
           <ul className="copyright">
-             <li>&copy; Copyright 2017 Nordic Giant</li>
-             <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+             <li>&copy; Copyright 2021 </li>
           </ul>
 
        </div>
